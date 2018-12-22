b72f389ff56bb0bb15143e685c5f34cca7ac7894
Ajar Vashisth <avashisth@semprautilities.com>
2018-12-22 12:57:56 -0800

    bug fix

--- jobs/jseq.sh
+++ jobs/jseq.sh
@@ -6,5 +6,5 @@ echo "Fetching Treasury Rates Data"
 python ${BASE}/j1.py --file ${BASE}/../data/treasury_yield.csv
 
 echo "Creating Charts"
-python ${BASE}/j2.py --in-csv --file ${BASE}/../data/treasury_yield.csv --out-png ${BASE}/../public/charts/treas_yield_3m_5y_30y.png
+python ${BASE}/j2.py --in-csv ${BASE}/../data/treasury_yield.csv --out-png ${BASE}/../public/charts/treas_yield_3m_5y_30y.png
 
