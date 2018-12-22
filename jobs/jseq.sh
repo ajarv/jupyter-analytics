56b70205036ca9a08f3082a892c307ba69be54b4
Ajar Vashisth <avashisth@semprautilities.com>
2018-12-22 13:38:42 -0800

    xoxo

--- jobs/jseq.sh
+++ jobs/jseq.sh
@@ -6,5 +6,6 @@ echo "Fetching Treasury Rates Data"
 python ${BASE}/j1.py --file ${BASE}/../data/treasury_yield.csv
 
 echo "Creating Charts"
+mkdir -p ${BASE}/../notebook/charts
 python ${BASE}/j2.py --in-csv ${BASE}/../data/treasury_yield.csv --out-png ${BASE}/../notebook/charts/ty_3m_5y_30y.png
 
